a3cf1d2d312b69853f25d9b7f4ab5143debff2d9
Brobss <gabrieldomingos13@gmail.com>
2023-04-11 15:50:06 -0300

    Ícones

--- App.js
+++ App.js
@@ -1,26 +1,88 @@
 import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Provider as PaperProvider } from "react-native-paper";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Image } from "react-native";
 
 import Home from "./screens/Home";
 import Livros from "./screens/Livros";
 import Filmes from "./screens/Filmes";
 import Series from "./screens/Series";
 
-const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
-  
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Filmes" component={Filmes} />
-      <Tab.Screen name="Livros" component={Livros} />
-      <Tab.Screen name="Séries" component={Series} />
+      <Tab.Screen
+        icon="home"
+        name="Home"
+        component={Home}
+        options={{
+          title: "Home",
+          tabBarIcon: ({ size, focused, color }) => {
+            return (
+              <Image
+                style={{ width: size, height: size }}
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/512/7133/7133312.png",
+                }}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Filmes"
+        component={Filmes}
+        options={{
+          title: "Filmes",
+          tabBarIcon: ({ size, focused, color }) => {
+            return (
+              <Image
+                style={{ width: size, height: size }}
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/512/1581/1581249.png",
+                }}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Livros"
+        component={Livros}
+        options={{
+          title: "Livros",
+          tabBarIcon: ({ size, focused, color }) => {
+            return (
+              <Image
+                style={{ width: size, height: size }}
+                source={{
+                  uri: "https://uxwing.com/wp-content/themes/uxwing/download/education-school/read-book-icon.png",
+                }}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Séries"
+        component={Series}
+        options={{
+          title: "Séries",
+          tabBarIcon: ({ size, focused, color }) => {
+            return (
+              <Image
+                style={{ width: size, height: size }}
+                source={{
+                  uri: "https://www.iconpacks.net/icons/2/free-movie-icon-2802-thumb.png",
+                }}
+              />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
@@ -32,18 +94,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-// export default function App() {
-//   return (
-//     <PaperProvider>
-//       <NavigationContainer>
-//         <Stack.Navigator>
-//           <Stack.Screen name="Home" component={Home} />
-//           <Stack.Screen name="Livros" component={Livros} />
-//           <Stack.Screen name="Series" component={Series} />
-//           <Stack.Screen name="Filmes" component={Filmes} />
-//         </Stack.Navigator>
-//       </NavigationContainer>
-//     </PaperProvider>
-//   );
-// }
